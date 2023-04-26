07888fb172d108507e2e83dfcead0dbbc65c7c8a
Jeremy Bindel <jeremy.bindel@etudiant.univ-rennes1.fr>
2023-04-26 10:00:36 +0200

    [Self generated memory] Add one activity and bound to the training menu.

--- BeFaster/app/src/main/java/com/kjnco/befaster/main_menu/TrainingActivity.kt
+++ BeFaster/app/src/main/java/com/kjnco/befaster/main_menu/TrainingActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.kjnco.befaster.R
 import com.kjnco.befaster.MainActivity
 import com.kjnco.befaster.quiz.QuizMenu
+import com.kjnco.befaster.self_generated_suite.SelfGenerated
 
 /**
  * Class that aims to offer a mode in solo for training
@@ -21,6 +22,7 @@ class TrainingActivity: AppCompatActivity() {
         val description2: TextView by lazy { findViewById(R.id.description_2)}
         val description3: TextView by lazy { findViewById(R.id.description_3)}
         val fastQuiz: Button by lazy { findViewById(R.id.fast_quiz)}
+        val selfGenerated: Button by lazy { findViewById(R.id.self_generated)}
         val cancelButton: Button by lazy {findViewById(R.id.cancel)}
 
         // Set the text of the buttons
@@ -28,11 +30,17 @@ class TrainingActivity: AppCompatActivity() {
         description2.setText(R.string.training_description_2)
         description3.setText(R.string.training_description_3)
         fastQuiz.setText(R.string.quiz)
+        selfGenerated.setText(R.string.self_generated_fr)
         cancelButton.setText(R.string.cancel)
 
         // Set the action of the buttons
         fastQuiz.setOnClickListener{
             val intent = Intent(this, QuizMenu::class.java)
+            startActivity(intent)
+        }
+
+        selfGenerated.setOnClickListener{
+            val intent = Intent(this, SelfGenerated::class.java)
             startActivity(intent)
         }
 
---
+++ BeFaster/app/src/main/java/com/kjnco/befaster/self_generated_suite/SelfGenerated.kt
@@ -0,0 +1,12 @@
+package com.kjnco.befaster.self_generated_suite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kjnco.befaster.R
+
+class SelfGenerated : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_self_generated)
+    }
+}
