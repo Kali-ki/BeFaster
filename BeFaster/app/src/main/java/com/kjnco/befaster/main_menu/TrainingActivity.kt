f5e658577c841191efc97a56fcaf93102e1292ed
Jeremy Bindel <jeremy.bindel@etudiant.univ-rennes1.fr>
2023-05-13 15:06:59 +0200

    Adding new activity and prepare the others to come

--- BeFaster/app/src/main/java/com/kjnco/befaster/main_menu/TrainingActivity.kt
+++ BeFaster/app/src/main/java/com/kjnco/befaster/main_menu/TrainingActivity.kt
@@ -13,41 +13,112 @@ import com.kjnco.befaster.self_generated_suite.SelfGenerated
  * Class that aims to offer a mode in solo for training
  */
 class TrainingActivity: AppCompatActivity() {
+
+    private lateinit var genCultureQuiz: Button
+    private lateinit var wordQuiz: Button
+    private lateinit var fillTheRest: Button
+    private lateinit var selfGenerated: Button
+    private lateinit var suiteToGenerate: Button
+    private lateinit var labyrinth: Button
+    private lateinit var cancelButton: Button
+
     override fun onCreate(savedInstanceState: android.os.Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_training)
 
-        // Get the element of the training activity
-        val description1: TextView by lazy { findViewById(R.id.description_1)}
-        val description2: TextView by lazy { findViewById(R.id.description_2)}
-        val description3: TextView by lazy { findViewById(R.id.description_3)}
-        val fastQuiz: Button by lazy { findViewById(R.id.fast_quiz)}
-        val selfGenerated: Button by lazy { findViewById(R.id.self_generated)}
-        val cancelButton: Button by lazy {findViewById(R.id.cancel)}
-
-        // Set the text of the buttons
-        description1.setText(R.string.training_description_1)
-        description2.setText(R.string.training_description_2)
-        description3.setText(R.string.training_description_3)
-        fastQuiz.setText(R.string.quiz)
-        selfGenerated.setText(R.string.self_generated_fr)
-        cancelButton.setText(R.string.cancel)
+        // Buttons init
+        initButtons()
 
         // Set the action of the buttons
-        fastQuiz.setOnClickListener{
+        genCultureQuizButtonEvent()
+        wordQuizButtonEvent()
+        fillTheRestButtonEvent()
+        selfGeneratedButtonEvent()
+        suiteToGenerateButtonEvent()
+        labyrinthButtonEvent()
+        cancelButtonEvent()
+    }
+
+    /**
+     * Method to add an event to the genCultureQuiz button
+     */
+    private fun genCultureQuizButtonEvent() {
+        genCultureQuiz.setOnClickListener {
             val intent = Intent(this, QuizMenu::class.java)
             startActivity(intent)
         }
+    }
 
-        selfGenerated.setOnClickListener{
+    /**
+     * Method to add an event to the wordQuiz button
+     */
+    private fun wordQuizButtonEvent() {
+        wordQuiz.setOnClickListener {
+            val intent = Intent(this, QuizMenu::class.java)
+            startActivity(intent)
+        }
+    }
+
+    /**
+     * Method to add an event to the fillTheRest button
+     */
+    private fun fillTheRestButtonEvent() {
+        fillTheRest.setOnClickListener {
+            val intent = Intent(this, QuizMenu::class.java)
+            startActivity(intent)
+        }
+    }
+
+    /**
+     * Method to add an event to the selfGenerated button
+     */
+    private fun selfGeneratedButtonEvent() {
+        selfGenerated.setOnClickListener {
             val intent = Intent(this, SelfGenerated::class.java)
             startActivity(intent)
         }
+    }
 
-        cancelButton.setOnClickListener{
+    /**
+     * Method to add an event to the suiteToGenerate button
+     */
+    private fun suiteToGenerateButtonEvent() {
+        suiteToGenerate.setOnClickListener {
+            val intent = Intent(this, QuizMenu::class.java)
+            startActivity(intent)
+        }
+    }
+
+    /**
+     * Method to add an event to the labyrinth button
+     */
+    private fun labyrinthButtonEvent() {
+        labyrinth.setOnClickListener {
+            val intent = Intent(this, QuizMenu::class.java)
+            startActivity(intent)
+        }
+    }
+
+    /**
+     * Method to add an event to the cancel button
+     */
+    private fun cancelButtonEvent() {
+        cancelButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+    }
 
+    /**
+     * Method to initialize the buttons
+     */
+    private fun initButtons() {
+        genCultureQuiz = findViewById(R.id.gen_culture_quiz)
+        wordQuiz = findViewById(R.id.word_quiz)
+        fillTheRest = findViewById(R.id.fill_the_rest)
+        selfGenerated = findViewById(R.id.self_generated)
+        suiteToGenerate = findViewById(R.id.suite_to_generate)
+        labyrinth = findViewById(R.id.labyrinth)
+        cancelButton = findViewById(R.id.cancel)
     }
 }
